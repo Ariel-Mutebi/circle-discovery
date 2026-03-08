2bbc081b60da74fdcf15ab39061135beeefddb26
black-RAM <pplanes311@gmail.com>
2026-03-08 14:02:09 +0300

    increase CONTRACTION_FACTOR from 0.4 to 0.8

--- src/utils/BFM.ts
+++ src/utils/BFM.ts
@@ -5,7 +5,7 @@ import { addPlacesToMap, coordinatesOfPlaces } from "./filters.js";
 const MAX_ITERATIONS = 10;
 const NOC_FACTOR = 0.86;
 const EXPANSION_FACTOR = 1.2;
-const CONTRACTION_FACTOR = 0.4;
+const CONTRACTION_FACTOR = 0.8;
 
 export function calculateBarycenter(points: LatLng[]): LatLng {
   if (points.length === 0) {
